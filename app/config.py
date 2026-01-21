eef4846b9bbadd0931d0d896eb2a0d7e6df23e23
Nazar-Immam <nazar.immam@gmail.com>
2026-01-21 08:42:25 +0530

    Add File Upload Validation of size and type allowed

---
+++ app/config.py
@@ -0,0 +1,7 @@
+MAX_FILE_SIZE = 5*1024*1024 # =5mb
+
+ALLOWED_CONTENT_TYPES = {  
+    "image/png",       #Specifc MIME Types
+    "image/jpeg",
+    "application/pdf"
+}
--- app/routers/files.py
+++ app/routers/files.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter , UploadFile , File , HTTPException
+from fastapi import APIRouter , UploadFile , File , HTTPException , status
+from ..config import MAX_FILE_SIZE , ALLOWED_CONTENT_TYPES
 
 router = APIRouter(
     prefix="/files",
@@ -7,7 +8,16 @@ router = APIRouter(
 
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
+
+    if file.content_type not in ALLOWED_CONTENT_TYPES:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="File type not Allowed")
+
     contents = await file.read()
+    
+    if len(contents) > MAX_FILE_SIZE :
+        raise HTTPException(status_code= status.HTTP_413_CONTENT_TOO_LARGE,
+                            detail="File too large, compress it to be under 5mb !")
 
     return {
         "filename": file.filename,
